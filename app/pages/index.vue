dc33bd041038230d58278d1bb8baeec3255a849d
Armverystrong <77956338+n0b8d1@users.noreply.github.com>
2026-04-19 21:47:24 +0800

    Update index.vue with a custom welcome message
    Replaced existing components with a welcome message.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex flex-col justify-center">
-    <HomeHero />
-    <HomeLogos />
-    <HomeFeatures />
-    <HomeStats />
-    <HomeTestimonials />
-    <HomeCta />
+    
+    <div style="text-align: center; margin-top: 50px;">
+      <h1>这是我的专属主页</h1>
+      <p>欢迎访问！</p>
+    </div>
+
   </div>
 </template>
